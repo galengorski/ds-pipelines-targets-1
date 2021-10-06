48f366bf2bb0cd396706ecbf77fea9899166a162
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2021-10-06 19:01:00 -0400

    commit empty folder

--- 1_fetch/src/fetch_data.R
+++ 1_fetch/src/fetch_data.R
@@ -3,6 +3,7 @@ library(sbtools)
 
 # Get the data from ScienceBase
 fetch_data <- function(){
+dir.create('1_fetch/out/')
 item_file_download('5d925066e4b0c4f70d0d0599', names = 'me_RMSE.csv', destinations = '1_fetch/out/model_RMSEs.csv', overwrite_file = TRUE)
 }
 
