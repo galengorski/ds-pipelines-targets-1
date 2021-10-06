8f5e50feb1ad071878d9b060e6d6dba8618cbf9c
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2021-10-06 18:39:36 -0400

    reorder file structure

--- 1_fetch/src/fetch_data.R
+++
@@ -1,8 +0,0 @@
-
-library(sbtools)
-
-# Get the data from ScienceBase
-fetch_data <- function(){
-item_file_download('5d925066e4b0c4f70d0d0599', names = 'me_RMSE.csv', destinations = '1_fetch/out/model_RMSEs.csv', overwrite_file = TRUE)
-}
-
